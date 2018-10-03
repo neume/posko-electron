800e29da61c7fbd0da160959cfba447d8e2aec1c
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-10-03 12:54:59 +0800

    Add main window

---
+++ index.js
@@ -0,0 +1,13 @@
+const electron = require('electron')
+const {app, BrowserWindow} = electron
+
+let win;
+
+app.on('ready', function(){
+  win = new BrowserWindow({});
+  win.loadFile('views/index.html')
+
+  win.on('closed', function () {
+    win = null
+  })
+})

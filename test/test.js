81dfba3229be10ccd469ac503f5fe065fcf8b758
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2019-04-21 16:58:52 +0800

    Add example test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var expect = require('expect.js');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      expect(1).to.be.ok();
+    });
+  });
+});

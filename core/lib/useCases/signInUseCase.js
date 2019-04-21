7d983cd991c246b8f0e566ee9a3bc9cb6698e33a
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2019-04-21 21:54:38 +0800

    Refactor request

--- core/lib/useCases/signInUseCase.js
+++ core/lib/useCases/signInUseCase.js
@@ -1,9 +1,13 @@
 import SignInRequest from '../requests/signInRequest'
 export default class SignInUseCase {
-  constructor(options = {
-    request: new SignInRequest({baseURL: 'http://localhost:3001'})
-  }) {
-    this.request = options.request
+  constructor( config = {}) {
+    let defaults = {
+      request: new SignInRequest({baseURL: 'http://localhost:3001'})
+    }
+
+    let merged_config = Object.assign(defaults, config)
+
+    this.request = merged_config.request
   }
   async perform(options = {}, callback) {
     let { account_name, email, password } = options,
